49596e88020aa86fc9f9a94d8257490ffc83768e
Rokusuke69 <chauhan.abhishek.motilal@gmail.com>
2025-11-01 00:34:07 +0530

    Added frontend build files in backend

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -18,7 +18,7 @@ const Register = () => {
         setSubmitting(true);
         console.log(form);
 
-        axios.post("http://localhost:3000/api/auth/register", {
+        axios.post("https://chat-gpt-phnu.onrender.com/api/auth/register", {
             email: form.email,
             fullName: {
                 firstName: form.firstname,
